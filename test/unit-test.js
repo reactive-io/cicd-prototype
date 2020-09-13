a619dd33f090e192b84cd2292aac203de0f600fb
Robert Sosinski <6429+robertsosinski@users.noreply.github.com>
2020-09-13 18:08:54 -0500

    updates to tests

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,10 +1,33 @@
-/* eslint-disable no-undef */
-
 var assert = require("assert");
-describe("Array", function () {
+
+before(function () {
+  console.log("starting test suite...");
+});
+
+after(function () {
+  console.log("ending test suite...");
+});
+
+describe("Array", function () {  
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.ok([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("String", function () {  
+  describe("#match()", function () {
+    it("find a substring within a string", function () {
+      assert.ok("start testing 123".match("testing"));
+    });
+  });
+});
+
+describe("boolean", function () {
+  describe("===", function () {
+    it("should consider true to be equal to true", function () {
+      assert.ok(true === true);
     });
   });
 });
